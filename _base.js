61501e5106138b9a4723ce62293e0babcb9d2f1f
Sean Whalley <swhalley@gmail.com>
2016-09-02 19:19:36 -0300

    create base example. From my school attendance repo

---
+++ _base.js
@@ -0,0 +1,25 @@
+function loadData( filePath ){
+    return new Promise( resolve => {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                let response = xhr.responseText;
+
+                let rawSchoolData = response.split(/\r\n|\n/).map( school => school.split( ',') );
+                let headerRows = rawSchoolData.slice(0, 1)[0].filter( lineItem => !!lineItem );
+                let schoolData = rawSchoolData.slice(1).map( school => school.slice( 0, headerRows.length));
+
+                schoolData.forEach( school => {
+                    for( var i=1; i < school.length; i++){
+                        school[i] = +school[i];
+                    } // Would rather do something like school.slice(1).map(Number)
+                });
+
+                resolve( { headerRows, schoolData });
+            }
+        };
+
+        xhr.open('GET', filePath);
+        xhr.send(null);
+    });
+};
